26810653a6241c376310c614fb89b349665c3d06
Filippo Guerra <catnip@Catnip>
2022-12-09 11:30:14 +0100

    Major modification. Random Starting Position added, UDP server fixed

--- task1.cc
+++ task1.cc
@@ -56,31 +56,27 @@ int main(int argc, char* argv[]){
     // Using WifiHelper
     WifiHelper wifi = WifiHelper();
     wifi.SetRemoteStationManager("ns3::AarfWifiManager");
-    wifi.SetStandard(ns3::WifiStandard::WIFI_STANDARD_80211b);
+    wifi.SetStandard(ns3::WifiStandard::WIFI_STANDARD_80211g);
 
     // MAC helper
     WifiMacHelper mac; mac.SetType("ns3::AdhocWifiMac");
+    /**
+     *      MAC addresses:
+     *  n0  00:00:00:00:00:01
+     *  n1  00:00:00:00:00:02
+     *  n2  00:00:00:00:00:03
+     *  n3  00:00:00:00:00:04
+     *  n4  00:00:00:00:00:05
+     *
+    **/
 
     // Installing wifi on the netcontainer
     NetDeviceContainer WifiDevices = wifi.Install(phy, mac, WifiContainer);
-    
-    for(int i=0; i<5; i++) cout << "Mac nodo" << i << " e':"<< WifiDevices.Get(i)->GetAddress()<<endl;
-    /**
-     *
-     *      MAC addresses:
-     *  n0  00:00:00:00:00:02
-     *  n1  00:00:00:00:00:03
-     *  n2  00:00:00:00:00:04
-     *  n3  00:00:00:00:00:05
-     *  n4  00:00:00:00:00:06
-     *
-     *
-    */
 
     MobilityHelper mobility;
     mobility.SetPositionAllocator("ns3::RandomRectanglePositionAllocator",
-        "X", ("ns3::UniformRandomVariable[Min=-90|Max=90]"),
-        "Y", StringValue("ns3::UniformRandomVariable[Min=-90|Max=90]")
+        "X", StringValue("ns3::UniformRandomVariable[Min=0|Max=90]"),
+        "Y", StringValue("ns3::UniformRandomVariable[Min=0|Max=90]")
     );
     mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
         "Bounds", RectangleValue(Rectangle(-90, 90, -90, 90))
@@ -96,7 +92,7 @@ int main(int argc, char* argv[]){
     /**
      *
      * Ipv4 addresses:
-     * nAp 192.168.1.1
+     * n0  192.168.1.1
      * n1  192.168.1.2
      * n2  192.168.1.3
      * n3  192.168.1.4
@@ -116,7 +112,6 @@ int main(int argc, char* argv[]){
     serverApps.Start(Seconds(0.0)); serverApps.Stop(Seconds(10.0));
 
     UdpEchoClientHelper n3_Client(n0_address, port);            // n3 sends to n0 (by its ipv4 address)
-    n3_Client.SetAttribute("MaxPackets", UintegerValue(1));
     n3_Client.SetAttribute("Interval", TimeValue(Seconds(2.0)));
     n3_Client.SetAttribute("PacketSize", UintegerValue(512));
     ApplicationContainer n3_ClientApp = n3_Client.Install(n3);
@@ -124,25 +119,19 @@ int main(int argc, char* argv[]){
     n3_ClientApp.Stop(Seconds(5.0));
 
     UdpEchoClientHelper n4_Client(n0_address, port);            // n4 sends to n0 (by its ipv4 address)
-    n4_Client.SetAttribute("MaxPackets", UintegerValue(1));
     n4_Client.SetAttribute("Interval", TimeValue(Seconds(1.0)));
     n4_Client.SetAttribute("PacketSize", UintegerValue(512));
     ApplicationContainer n4_ClientApp = n4_Client.Install(n4);
     n4_ClientApp.Start(Seconds(1.0));
-    n4_ClientApp.Stop(Seconds(2.0)); //TODO: Check if the second packet gets sent
+    n4_ClientApp.Stop(Seconds(2.5)); //Second packets gets sent and then stops (before third one)
 
-
-    // TODO : Check after this
-
-    string rtslimit ="1000";//ogni pacchetto minore di questo valore sarà trasmesso senza rts/cts
-    string state="off";
+    string state = "off";
     if(useRtsCts){
-        rtslimit="0"; //quindi se lo mettiamo a zero tutti i pacchetti saranno trasmessi con rts/cts
-        state="on";
+        string rtslimit = "0"; //quindi se lo mettiamo a zero tutti i pacchetti saranno trasmessi con rts/cts
+        state = "on";
+        Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue(rtslimit));
     }
     
-    Config::SetDefault("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue(rtslimit));
-
     phy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
     phy.EnablePcap("task2-n4.pcap", WifiDevices.Get(3),true,true);
 
@@ -152,56 +141,42 @@ int main(int argc, char* argv[]){
         Simulator::Run();
         Simulator::Destroy();
         NS_LOG_INFO("Done.");
-    }
-    else{
-        string pack_name="wireless-task1-rts-"+state+ ".xml";
+    } else{
+        string pack_name = "wireless-task1-rts-" + state + ".xml";
         AnimationInterface anim(pack_name);
-        uint32_t  nId_array[5];
-        for(int i=0; i<5;i++){
-            int n=WifiContainer.Get(i)->GetId();
-            nId_array[i]=n;
-        }
+        uint32_t ids[5];
+        for(int i=0; i<5;i++) ids[i] = WifiContainer.Get(i)->GetId();
 
         //N0 EchoServer
-        string n0_ID =to_string(nId_array[0]);
-        string n0_Anim_Name="SRV-" + n0_ID;
-    
-        anim.UpdateNodeDescription (n0, n0_Anim_Name);
-        anim.UpdateNodeColor (n0, 255, 0, 0);   //RED
+        string n0_Anim_Name = "SRV-" + to_string(ids[0]);
+        anim.UpdateNodeDescription(n0, n0_Anim_Name);
+        anim.UpdateNodeColor(n0, 255, 0, 0);   //RED
     
         //N3 Echoclient
-        string n3_ID =to_string(nId_array[3]);
-        string n3_Anim_Name= "CLI-"+n3_ID;
-
-        anim.UpdateNodeDescription (n3, n3_Anim_Name);
-        anim.UpdateNodeColor (n3, 0, 255, 0); 
+        string n3_Anim_Name= "CLI-" + to_string(ids[3]);
+        anim.UpdateNodeDescription(n3, n3_Anim_Name);
+        anim.UpdateNodeColor(n3, 0, 255, 0); 
 
         //N4 Echoclient
-        string n4_ID =to_string(nId_array[4]);
-        string n4_Anim_Name= "CLI-"+n4_ID;
-
+        string n4_Anim_Name= "CLI-" + to_string(ids[4]);
         anim.UpdateNodeDescription (n4, n4_Anim_Name);
         anim.UpdateNodeColor (n4, 0, 255, 0); //GREEN
 
         //N1 node
-        string n1_ID =to_string(nId_array[1]);
-        string n1_Anim_Name= "STA-"+n1_ID;
-
+        string n1_Anim_Name= "HOC-" + to_string(ids[1]);
         anim.UpdateNodeDescription (n1, n1_Anim_Name);
         anim.UpdateNodeColor (n1, 0, 0, 255); 
 
         //N2 node
-        string n2_ID =to_string(nId_array[2]);
-        string n2_Anim_Name= "STA-"+n2_ID;
-
+        string n2_Anim_Name= "HOC-" + to_string(ids[2]);
         anim.UpdateNodeDescription (n2, n2_Anim_Name);
         anim.UpdateNodeColor (n2, 0, 0, 255); //BLUE
 
         anim.EnablePacketMetadata(); // Optional
         anim.EnableIpv4RouteTracking("routingtable-wireless.xml",
-                                 Seconds(0),  //inizio
-                                 Seconds(5),   //fine
-                                 Seconds(0.25));     //intervallo    
+            Seconds(0),     //inizio
+            Seconds(5),     //fine
+            Seconds(0.25)); //intervallo
         anim.EnableWifiMacCounters(Seconds(0), Seconds(10)); // Optional
         anim.EnableWifiPhyCounters(Seconds(0), Seconds(10)); // Optional
         
@@ -212,6 +187,5 @@ int main(int argc, char* argv[]){
         NS_LOG_INFO("Done.");
     }
 
-    return 0;
     return 0;
 }
